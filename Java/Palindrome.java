690b4bf943483528a120ef286e27a9a42c22fd9b
Colisian McLeod <mcleodom16@gmail.com>
2023-11-01 12:57:00 -0400

    Added to notes.

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -9,7 +9,7 @@ public class Palindrome {
         int reverse = 0; //storing the value we will reflect the inputted number 
         int holder = x; //Holder will be the value of the number enterring the loop with the last digit removed from the previous loop 
 
-        while(holder != 0){ //Only single integer so while loop will suffice
+        while(holder != 0){ //Only single integer will be the evaulated not an array so while loop will suffice
             int lastDigit = holder % 10; // this will isolate the last digit of of the number entered (remainder) 
             reverse = reverse * 10 + lastDigit; //This equation will reverse the integer digit by digit after each loop
             holder /= 10; // updates holder to be the integer with its last digit removed
