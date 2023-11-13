899ab3594acd93d0123cc5f47d38aacf1c2aee30
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2023-11-13 10:53:09 -0500

    Recursion python attempt.

---
+++ Python/recursion.py
@@ -0,0 +1,12 @@
+def countdown(x):
+    if x == 0:
+        print("done")
+        return
+    else:
+        print(x, "...")
+        countdown(x-1) #function call
+        print("Stack Check") #call stack will be unwound after the final return statement. Print statement will be called for each item in the call stack
+
+
+
+countdown(5)
