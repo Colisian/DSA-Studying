ab54ac55a8ed42509d32ae4499c7d2e3fe3a8f04
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2023-11-20 17:19:55 -0500

    Added changes to mergeSort.

--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -3,17 +3,17 @@ items = [6,20,8,19,56,23,87,41,49,53]
 
 def mergeSort(dataset):
     if len(dataset) > 1: ##breaking condition until single elements
-        mid = len(dataset) //2 #finding the midpoint
+        mid = len(dataset) // 2 #finding the midpoint
         leftArr = dataset[:mid]
         rightArr = dataset[mid:]
 
-        mergeSort(leftArr) ## recursively breakdown the left arrays
-        mergeSort(rightArr) ## recursively breakdown the right  arrays
+        mergeSort(leftArr) # recursively breakdown the left arrays
+        mergeSort(rightArr) # recursively breakdown the right  arrays
 
 
-        i=0 ##index into left array
-        j=0 ##index into right array
-        k=0 ##index into merged array
+        i=0 #index into left array
+        j=0 #index into right array
+        k=0 #index into merged array
 
         while i < len(leftArr) and j < len(rightArr): # while each array has values
             if leftArr[i] < rightArr[j]:
@@ -23,7 +23,6 @@ def mergeSort(dataset):
                 dataset[k] = rightArr[j]
                 j+=1
             k += 1
-        
         
         while i < len(leftArr): #if there are left over values in left array add them in
             dataset[k] = leftArr[i]
